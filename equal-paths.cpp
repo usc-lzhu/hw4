5c068896ac1bfd00a52605a2ce0bd09158100258
Liana Zhu <lczhu@usc.edu>
2024-03-19 15:44:09 -0700

    equal paths version 1

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,27 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int equalPathsHelper(Node * node) {
+    // base clase: traversed past leaf node
+    if (node == nullptr) {
+        return 0;
+    }
+
+    if (equalPathsHelper(node -> left) == equalPathsHelper(node -> right)) {
+        return 1;
+    }
+}
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
+    if (equalPathsHelper(root)) {
+        return true;
+    }
+
+    return false;
+    
 }
 
--- equal-paths.h
+++ equal-paths.h
@@ -29,4 +29,6 @@ struct Node {
  */
 bool equalPaths(Node * root);
 
+int equalPathsHelper(Node * node);
+
 #endif
